0e8f31f5ce31e87b95406a5eea66661969377b3b
amscu-dev <amarinescu.dev@gmail.com>
2025-03-05 10:25:58 +0200

    Add user-button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,18 +1,23 @@
-import { auth, signOut } from "@/auth";
+"use client";
 
-async function SettingsPage() {
-  const session = await auth();
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { useSession, signOut } from "next-auth/react";
+
+function SettingsPage() {
+  // Pentru a folosi sesiunea pe client :
+  // const session = useSession();
+  // Pentru a folosi sesiunea pe server : auth()
+  // Pe client avem signOut din: next-auth/react
+  // Putem implementa log-out si pe server, cu server action - similar cu login
+  const onClick = () => {
+    signOut();
+  };
+  const user = useCurrentUser();
   return (
-    <div>
-      {JSON.stringify(session)}
-      <form
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: "/auth/login" });
-        }}
-      >
-        <button type="submit">Sign Out</button>
-      </form>
+    <div className="bg-white p-10 rounded-xl">
+      <button type="submit" onClick={onClick}>
+        Sign Out
+      </button>
     </div>
   );
 }

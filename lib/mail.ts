765020a1c784ba93345893b6a3582cc3a83781e6
amscu-dev <amarinescu.dev@gmail.com>
2025-03-25 13:19:31 +0200

    3: add domain to mail service

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ const domain = process.env.NEXT_PUBLIC_APP_URL;
 export const sentVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@nextauthv5-complete-toolkit.xyz",
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
@@ -16,7 +16,7 @@ export const sentVerificationEmail = async (email: string, token: string) => {
 export const sentPasswordResetEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-password?token=${token}`;
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@nextauthv5-complete-toolkit.xyz",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${confirmLink}">here</a> to reset your password.</p>`,
@@ -25,7 +25,7 @@ export const sentPasswordResetEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorEmail = async (email: string, token: string) => {
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "mail@nextauthv5-complete-toolkit.xyz",
     to: email,
     subject: "2FA Code",
     html: `<p>Here its your 2FA code: ${token}</p>`,
